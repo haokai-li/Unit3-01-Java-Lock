a0a1d9f462058b0cb15eaff43d7887e9d3392359
haokaili <82911890+haokai-li@users.noreply.github.com>
2021-12-08 02:41:19 +0000

    11

--- Main.java
+++ Main.java
@@ -20,15 +20,15 @@ class Main {
         aStack.showStack();
 
         System.out.println("Push: 1");
-    	aStack.push(1);
+        aStack.push(1);
         aStack.showStack();
 
         System.out.println("Push: 2");
-    	aStack.push(2);
+        aStack.push(2);
         aStack.showStack();
 
         System.out.println("Push: 3");
-    	aStack.push(THREE);
+        aStack.push(THREE);
         aStack.showStack();
 
         System.out.println("\nDone.");
