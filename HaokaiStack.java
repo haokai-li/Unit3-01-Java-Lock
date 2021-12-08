8ffc9063de17a187f843b71266e9a56c7a15fbcb
haokaili <82911890+haokai-li@users.noreply.github.com>
2021-12-08 02:10:16 +0000

    6

--- HaokaiStack.java
+++ HaokaiStack.java
@@ -13,9 +13,6 @@ import java.util.ArrayList;
 * This is the standard "Towers of Hanoi" program.
 */
 public class HaokaiStack {
-    /**
-    * arraylist
-    */
     private ArrayList<Integer> stackAsArray = new ArrayList<Integer>();
 
     /**
